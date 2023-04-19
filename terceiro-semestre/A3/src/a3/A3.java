e8f6aabd7d25410ee84919308dba421095ec5912
Gustavo Gino Terezo <gustavogino@outlook.com.br>
2023-04-19 11:54:00 -0300

    Commit de alteração

--- terceiro-semestre/A3/src/a3/A3.java
+++ terceiro-semestre/A3/src/a3/A3.java
@@ -17,6 +17,7 @@ public class A3 {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("Olá, mundo!");
+        System.out.println("Gustavo");
     }
     
 }
