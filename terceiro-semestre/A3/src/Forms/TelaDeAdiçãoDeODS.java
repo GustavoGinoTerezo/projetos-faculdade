b90067f5da282d294193ee1f9bb18cc6fa709b62
822158424 <822158424@MCAHLI0718.animaedu.intranet>
2023-04-27 11:52:57 -0300

    27/04/2023 11:05 Telas de ODS - Concluida

--- terceiro-semestre/A3/src/Forms/TelaDeAdiçãoDeODS.java
+++ terceiro-semestre/A3/src/Forms/TelaDeAdiçãoDeODS.java
@@ -27,21 +27,260 @@ public class TelaDeAdiçãoDeODS extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        jComboBox1 = new javax.swing.JComboBox<>();
+        BtnCadastrar_se = new javax.swing.JButton();
+        BtnCadastrar_se1 = new javax.swing.JButton();
+        TxtEmail = new javax.swing.JTextField();
+        jLabel3 = new javax.swing.JLabel();
+        jPanel1 = new javax.swing.JPanel();
+        jLabel4 = new javax.swing.JLabel();
+        TxtEmail1 = new javax.swing.JTextField();
+        jLabel5 = new javax.swing.JLabel();
+        TxtEmail2 = new javax.swing.JTextField();
+        jLabel6 = new javax.swing.JLabel();
+        jLabel7 = new javax.swing.JLabel();
+        jLabel8 = new javax.swing.JLabel();
+        TxtDia = new javax.swing.JTextField();
+        TxtMes = new javax.swing.JTextField();
+        TxtAno = new javax.swing.JTextField();
+        jLabel9 = new javax.swing.JLabel();
+        TxtEmail4 = new javax.swing.JTextField();
+        jComboBox2 = new javax.swing.JComboBox<>();
+
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setTitle("Cadastro de Projeto de ODS");
+
+        jComboBox1.setFont(new java.awt.Font("Tahoma", 1, 14)); // NOI18N
+        jComboBox1.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Erradicação da pobreza", "Erradicação da fome", "Saúde e Bem-Estar", "Educação de qualidade", "Igualdade de gênero", "Água Potável e Saneamento", "Energia acessível e limpa", "Trabalho decente e crescimento econômico", "Inovação e infraestrutura", "Redução das desigualdades", "Cidades e comunidades sustentáveis", "Consumo e produção responsáveis", "Ação contra a Mudança Global do Clima", "Vida na Água", "Vida Terrestre", "Paz, Justiça e Instituições Eficazes", "Parcerias e Meios de Implementação" }));
+
+        BtnCadastrar_se.setBackground(new java.awt.Color(255, 255, 255));
+        BtnCadastrar_se.setFont(new java.awt.Font("Arial", 1, 18)); // NOI18N
+        BtnCadastrar_se.setText("Cadastrar");
+        BtnCadastrar_se.setBorder(new javax.swing.border.LineBorder(new java.awt.Color(0, 0, 0), 1, true));
+        BtnCadastrar_se.setPreferredSize(new java.awt.Dimension(120, 24));
+        BtnCadastrar_se.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                BtnCadastrar_seMouseClicked(evt);
+            }
+        });
+        BtnCadastrar_se.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                BtnCadastrar_seActionPerformed(evt);
+            }
+        });
+
+        BtnCadastrar_se1.setBackground(new java.awt.Color(255, 255, 255));
+        BtnCadastrar_se1.setFont(new java.awt.Font("Arial", 1, 18)); // NOI18N
+        BtnCadastrar_se1.setText("Cancelar");
+        BtnCadastrar_se1.setBorder(new javax.swing.border.LineBorder(new java.awt.Color(0, 0, 0), 1, true));
+        BtnCadastrar_se1.setPreferredSize(new java.awt.Dimension(120, 24));
+        BtnCadastrar_se1.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                BtnCadastrar_se1MouseClicked(evt);
+            }
+        });
+        BtnCadastrar_se1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                BtnCadastrar_se1ActionPerformed(evt);
+            }
+        });
+
+        TxtEmail.setFont(new java.awt.Font("Arial", 0, 14)); // NOI18N
+
+        jLabel3.setFont(new java.awt.Font("Tahoma", 1, 18)); // NOI18N
+        jLabel3.setText("Código do Projeto");
+
+        jPanel1.setBackground(new java.awt.Color(3, 175, 239));
+        jPanel1.setBorder(javax.swing.BorderFactory.createEtchedBorder());
+
+        jLabel4.setBackground(new java.awt.Color(3, 175, 239));
+        jLabel4.setFont(new java.awt.Font("Arial", 1, 36)); // NOI18N
+        jLabel4.setText("Cadastro de Projeto de ODS");
+        jLabel4.setBorder(javax.swing.BorderFactory.createEtchedBorder());
+
+        javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
+        jPanel1.setLayout(jPanel1Layout);
+        jPanel1Layout.setHorizontalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addComponent(jLabel4)
+                .addContainerGap())
+        );
+        jPanel1Layout.setVerticalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addComponent(jLabel4)
+                .addContainerGap())
+        );
+
+        TxtEmail1.setFont(new java.awt.Font("Arial", 0, 14)); // NOI18N
+
+        jLabel5.setFont(new java.awt.Font("Tahoma", 1, 18)); // NOI18N
+        jLabel5.setText("Descrição do Projeto");
+
+        TxtEmail2.setFont(new java.awt.Font("Arial", 0, 14)); // NOI18N
+
+        jLabel6.setFont(new java.awt.Font("Tahoma", 1, 18)); // NOI18N
+        jLabel6.setText("Responsável do Projeto");
+
+        jLabel7.setFont(new java.awt.Font("Tahoma", 1, 18)); // NOI18N
+        jLabel7.setText("Telefone do Responsável");
+
+        jLabel8.setFont(new java.awt.Font("Tahoma", 1, 18)); // NOI18N
+        jLabel8.setText("Data de Criação");
+
+        TxtDia.setFont(new java.awt.Font("Arial", 0, 14)); // NOI18N
+        TxtDia.setHorizontalAlignment(javax.swing.JTextField.CENTER);
+        TxtDia.setText("Dia");
+        TxtDia.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                TxtDiaActionPerformed(evt);
+            }
+        });
+
+        TxtMes.setFont(new java.awt.Font("Arial", 0, 14)); // NOI18N
+        TxtMes.setHorizontalAlignment(javax.swing.JTextField.CENTER);
+        TxtMes.setText("Mes");
+        TxtMes.setPreferredSize(new java.awt.Dimension(50, 23));
+        TxtMes.addFocusListener(new java.awt.event.FocusAdapter() {
+            public void focusGained(java.awt.event.FocusEvent evt) {
+                TxtMesFocusGained(evt);
+            }
+        });
+
+        TxtAno.setFont(new java.awt.Font("Arial", 0, 14)); // NOI18N
+        TxtAno.setHorizontalAlignment(javax.swing.JTextField.CENTER);
+        TxtAno.setText("Ano");
+        TxtAno.setPreferredSize(new java.awt.Dimension(50, 23));
+        TxtAno.addFocusListener(new java.awt.event.FocusAdapter() {
+            public void focusGained(java.awt.event.FocusEvent evt) {
+                TxtAnoFocusGained(evt);
+            }
+        });
+
+        jLabel9.setFont(new java.awt.Font("Tahoma", 1, 18)); // NOI18N
+        jLabel9.setText("Status do Projeto");
+
+        TxtEmail4.setFont(new java.awt.Font("Arial", 0, 14)); // NOI18N
+
+        jComboBox2.setFont(new java.awt.Font("Tahoma", 1, 14)); // NOI18N
+        jComboBox2.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Não Iniciado", "Em andamento", "Concluido" }));
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 400, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(0, 0, Short.MAX_VALUE))
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                    .addGroup(layout.createSequentialGroup()
+                        .addComponent(jLabel9)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                        .addComponent(jComboBox2, 0, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                    .addComponent(jLabel7)
+                    .addComponent(TxtEmail)
+                    .addComponent(jLabel3)
+                    .addComponent(jComboBox1, 0, 381, Short.MAX_VALUE)
+                    .addComponent(jLabel6)
+                    .addComponent(jLabel5)
+                    .addComponent(TxtEmail2)
+                    .addComponent(TxtEmail1)
+                    .addGroup(layout.createSequentialGroup()
+                        .addComponent(jLabel8)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(TxtDia, javax.swing.GroupLayout.PREFERRED_SIZE, 35, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(5, 5, 5)
+                        .addComponent(TxtMes, javax.swing.GroupLayout.PREFERRED_SIZE, 52, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(TxtAno, javax.swing.GroupLayout.PREFERRED_SIZE, 45, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addComponent(TxtEmail4))
+                .addGap(61, 61, 61))
+            .addGroup(layout.createSequentialGroup()
+                .addGap(128, 128, 128)
+                .addComponent(BtnCadastrar_se, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addComponent(BtnCadastrar_se1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 300, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(31, 31, 31)
+                .addComponent(jComboBox1, javax.swing.GroupLayout.PREFERRED_SIZE, 31, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(18, 18, 18)
+                .addComponent(jLabel3)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(TxtEmail, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addComponent(jLabel5)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(TxtEmail1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addComponent(jLabel6)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(TxtEmail2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(jLabel7)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(TxtEmail4, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jLabel8)
+                    .addComponent(TxtMes, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(TxtAno, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .addComponent(TxtDia))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jLabel9)
+                    .addComponent(jComboBox2, javax.swing.GroupLayout.PREFERRED_SIZE, 31, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(18, 18, 18)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(BtnCadastrar_se, javax.swing.GroupLayout.PREFERRED_SIZE, 38, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(BtnCadastrar_se1, javax.swing.GroupLayout.PREFERRED_SIZE, 38, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(42, 42, 42))
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void BtnCadastrar_seMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_BtnCadastrar_seMouseClicked
+
+    }//GEN-LAST:event_BtnCadastrar_seMouseClicked
+
+    private void BtnCadastrar_seActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_BtnCadastrar_seActionPerformed
+
+      
+    }//GEN-LAST:event_BtnCadastrar_seActionPerformed
+
+    private void BtnCadastrar_se1MouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_BtnCadastrar_se1MouseClicked
+        // TODO add your handling code here:
+    }//GEN-LAST:event_BtnCadastrar_se1MouseClicked
+
+    private void BtnCadastrar_se1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_BtnCadastrar_se1ActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_BtnCadastrar_se1ActionPerformed
+
+    private void TxtMesFocusGained(java.awt.event.FocusEvent evt) {//GEN-FIRST:event_TxtMesFocusGained
+
+        TxtMes.setText("");
+
+    }//GEN-LAST:event_TxtMesFocusGained
+
+    private void TxtAnoFocusGained(java.awt.event.FocusEvent evt) {//GEN-FIRST:event_TxtAnoFocusGained
+
+        TxtAno.setText("");
+
+    }//GEN-LAST:event_TxtAnoFocusGained
+
+    private void TxtDiaActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_TxtDiaActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_TxtDiaActionPerformed
 
     /**
      * @param args the command line arguments
@@ -79,5 +318,24 @@ public class TelaDeAdiçãoDeODS extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton BtnCadastrar_se;
+    private javax.swing.JButton BtnCadastrar_se1;
+    public static javax.swing.JTextField TxtAno;
+    public static javax.swing.JTextField TxtDia;
+    public static javax.swing.JTextField TxtEmail;
+    public static javax.swing.JTextField TxtEmail1;
+    public static javax.swing.JTextField TxtEmail2;
+    public static javax.swing.JTextField TxtEmail4;
+    public static javax.swing.JTextField TxtMes;
+    private javax.swing.JComboBox<String> jComboBox1;
+    private javax.swing.JComboBox<String> jComboBox2;
+    private javax.swing.JLabel jLabel3;
+    private javax.swing.JLabel jLabel4;
+    private javax.swing.JLabel jLabel5;
+    private javax.swing.JLabel jLabel6;
+    private javax.swing.JLabel jLabel7;
+    private javax.swing.JLabel jLabel8;
+    private javax.swing.JLabel jLabel9;
+    private javax.swing.JPanel jPanel1;
     // End of variables declaration//GEN-END:variables
 }
